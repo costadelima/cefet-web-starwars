83aa355a9944b0e5b6bcd00ba12970b1e731f212
luiz flavio <constadelima@gmail.com>
2018-09-25 11:57:25 -0300

    exercicio 2 feito

--- starwars.js
+++ starwars.js
@@ -3,3 +3,30 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+function chama_episodio(e){
+  
+}
+$.ajax({
+  url: 'https://swapi.co/api/films/',
+  dataType: 'json',
+  success: function(resposta) {
+    // escreve a resposta no console (para ver o que foi recebido)
+    console.log(resposta);
+    let filme = resposta.results;
+
+    // console.log(filme[0].title);
+
+    resposta.results.forEach(adiciona);
+
+    function adiciona(filme){
+        console.log(filme.title);
+
+        let $li = $('<li></li>')
+        $li.html('Episódio ' + filme.episode_id + ': ' + filme.title);
+        $li.appendTo('#movies ul');
+        $li.data("data-episode-url", 'http://swapi.co/api/films/' + filme.episode_id);
+
+        $li.click(chama_episodio);
+    }
+  }
+});
